a42a61b209c9059a04075aface3a9862ccfb5f70
playmate69 <kfchotwings@live.nl>
2019-09-24 21:29:13 +0000

    spritesheets first steps

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 let fr = 60;
 var skeleton_idle;
 var skeleton_idle_spritesheet;
-var i;
 var skel_enemy;
 var level_one;
 var ground_level = 100;
 
 function preload() {
     skeleton_idle = loadImage('media/spritesheets/enemies/skeleton/idle.png');
-    skeleton_idle_spritesheet = skeleton_idle.get(0, 24, 24, 32);
 }
 
 function setup() {
     var canvas = createCanvas(1200, 800);
     canvas.parent("processing");
     frameRate(fr);
+        for(var i; i < fr; i+=25) {
+            skeleton_idle_spritesheet = skeleton_idle.get(i, 0, 24, 32);
+        }
     level_one = new Level_one();
     skel_enemy = new Skel_enemy();
 }
@@ -41,30 +42,16 @@ function Level_one(){
 }
 
 function Skel_enemy(){
-    this.width = 100;
-    this.height = 100;
+    this.width = 24 * 3;
+    this.height = 32 * 3;
 	this.x = 100;
 	this.y = height - ground_level - this.height;
 
 	this.display = function(){
         noStroke();
         fill('black');
-        rect(0, height-ground_level, 20);
+        //rect(this.x, height-ground_level-this.height, this.width, this.height);
         image(skeleton_idle_spritesheet, this.x, this.y, this.width, this.height);
+
 	}
-
-	this.move = function(){
-        this.y = this.y += -4;
-
-        if(this.y < 0 - this.height) {
-            this.x = random(0, width - this.width);
-            this.y = height;
-            if(scoreBoard.score > 0) {
-                scoreBoard.fout ++;
-            }
-        }
-    }
 }
-
-
-
