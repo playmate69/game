28ba556771aa351cd59f52667429b3260d341b92
playmate69 <kfchotwings@live.nl>
2019-11-27 00:08:48 +0000

    checkneighbour bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let fr = 30;
 var rows, cols;
 var grid = [];
 var w = 80;
+var current;
 //setup
 function setup() {
     var canvas = createCanvas(800, 800);
@@ -9,13 +10,13 @@ function setup() {
     frameRate(fr);
     rows = width/w;
     cols = width/w;
-
     for(var i=0;i<rows;i++) {
         for(var j=0;j<cols;j++) {
             cell = new Cell(i,j);
             grid.push(cell);
         }
     }
+    current = grid[0];
 }
 
 //draw
@@ -24,21 +25,57 @@ function draw() {
     for(var i=0;i<grid.length;i++) {
         grid[i].display();
     }
+    current.visited = true;
+    current.checkNeighbors();
 }
 
-function Cell(x, y) {
-    this.x = x;
-    this.y = y;
+function index(i, j) {
+    return i + j * cols;
+}
+
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+    this.walls = [true, true, true, true];
+    this.visited = false;
 
     this.display = function() {
-        var x = this.x*w;
-        var y = this.y*w;
+        var x = this.i*w;
+        var y = this.j*w;
         //console.log("(" + x + "," + y + ')');
-        stroke('black');
-        noFill();
-        line(x,y,x+w,y);
-        line(x+w,y,x+w,y+w);
-        line(x+w,y+w,x,y+w);
-        line(x,y+w,x,y);
+        if(this.visited) {
+            fill(195, 4, 4);
+            rect(x,y,w,w);
+        } else {
+            stroke('black');
+            noFill();
+        }
+        if(this.walls[0]) {
+            line(x,y,x+w,y);
+        }
+        if(this.walls[1]) {
+            line(x+w,y,x+w,y+w);
+        }
+        if(this.walls[2]) {   
+            line(x+w,y+w,x,y+w);
+        }
+        if(this.walls[3]) {
+            line(x,y+w,x,y);
+        }
+    }
+
+    this.checkNeighbors = function() {
+        var neighbors = [];
+        var top     = grid[index(i, j-1)],
+            right   = grid[index(i+1, j)],
+            bottom  = grid[index(i, j+1)],
+            left    = grid[index(i-1, j)];
+        var cordinates = [right, bottom];
+        for(var i=0;i<cordinates.length;i++) {
+            if(!cordinates[i].visited) {
+                neighbors.push(cordinates[i]);
+            }
+        }
+        console.log(neighbors);
     }
 }
